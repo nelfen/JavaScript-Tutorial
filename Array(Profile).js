86aefe04b9ea4247aed10dc0174314cdbe532c20
nelfen <nelfen.dev@gmail.com>
2022-04-25 00:53:38 +0900

    Array 예제(객체 생성)

---
+++ Array(Profile).js
@@ -0,0 +1,22 @@
+const player = {
+    name :  "Sian",
+    age : 23,
+    job : "Developer",
+    marryed : false,
+};
+
+console.log(player);
+
+console.log(player.marryed);
+
+player.marryed = true;
+
+console.log(player.marryed);
+
+player.point = 15;
+
+console.log(player);
+
+player.point = player.point + 15;
+
+console.log(player);
