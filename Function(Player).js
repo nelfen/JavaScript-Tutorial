b9295bccf9e7c3b228c347bddf45b91e62e2551e
nelfen <nelfen.dev@gmail.com>
2022-04-25 00:54:27 +0900

    Function 예제(객체 생성)

---
+++ Function(Player).js
@@ -0,0 +1,10 @@
+const player = {
+    name: "sian",
+    sayHello: function (OtherPersonName) {
+        console.log("Hello " + OtherPersonName + ", Nice to meet you!");
+    }
+}
+
+player.sayHello("Lynn");
+player.sayHello("Sian");
+player.sayHello("Jake");
