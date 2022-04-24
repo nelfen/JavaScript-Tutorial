b3b27806aae3ee82b63d54d8a24db51557de54c7
nelfen <nelfen.dev@gmail.com>
2022-04-24 23:17:37 +0900

    Const와 Let의 차이점

---
+++ ConstAndLet.js
@@ -0,0 +1,15 @@
+const myName = "Sian";  //수정 불가능
+let myAge = 23;         //수정 가능
+var myHobby = null;     //Var는 사용하지 않는 것이 좋음
+
+console.log(myName);
+console.log(myAge);
+console.log(myHobby);
+
+//myName = "Siu";
+myAge = 24;
+myHobby = "Coding"
+
+console.log(myName);
+console.log(myAge);
+console.log(myHobby);
