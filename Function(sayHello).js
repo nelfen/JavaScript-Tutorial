8e464e8cd7a6184618117b367fef07115c861e36
nelfen <nelfen.dev@gmail.com>
2022-04-25 00:54:14 +0900

    Function 예제(기본)

---
+++ Function(sayHello).js
@@ -0,0 +1,7 @@
+function sayHello(nameOfperson, age) {
+    console.log("Hello my name is " + nameOfperson + " and I'm " + age);
+}
+
+sayHello("Sian", 23);
+sayHello("Lyn", 20);
+sayHello("Jack", 32);
