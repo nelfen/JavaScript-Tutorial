7997caadbed37f83fb472976db03eb56c8ec59be
nelfen <nelfen.dev@gmail.com>
2022-05-02 19:06:41 +0900

    JS에서 HTML 정보 불러오기

---
+++ FirstJS/Chapter02/HTMLEdi(More).js
@@ -0,0 +1,7 @@
+//const hellos = document.getElementsByClassName("hello");
+//const title = document.getElementsByTagName("h1");
+//const title = document.querySelectorAll("div h1");
+const title = document.querySelector(".hello h1");
+
+//console.log(title);
+title.innerText = "Hello";
