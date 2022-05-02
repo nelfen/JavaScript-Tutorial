befc1028b5f28be9056986a2d08a3b6b2519b273
nelfen <nelfen.dev@gmail.com>
2022-05-02 17:57:58 +0900

    나중에 변경

--- FirstJS/Chapter02/App.js
+++ FirstJS/Chapter02/App.js
@@ -1,1 +1,9 @@
-console.log("ㅁ");
+const title = document.getElementById("title");
+
+//console.log(title);
+//console.dir(title);
+
+title.innerText = "Got you!";
+
+console.log(title.id);
+console.log(title.className);
