73654e92f51f79079b3c2e11c6476af72ef8a9af
nelfen <nelfen.dev@gmail.com>
2022-05-03 22:16:26 +0900

    Event 예제 - mouse Function

---
+++ FirstJS/Chapter02/Event(mouse).js
@@ -0,0 +1,19 @@
+const title = document.querySelector(".hello:first-child h1");
+
+console.dir(title);
+
+function handleTitleClick() {
+    title.style.color = "blue";
+}
+
+function handleMouseEnter() {
+    title.innerText = "Mouse is here!"
+}
+
+function handleMouseLeave() {
+    title.innerText = "Mouse is gond!"
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
