71de6d3fdbf26e0e6725ff1aa8630e4c1204cd8c
nelfen <nelfen.dev@gmail.com>
2022-05-16 23:10:14 +0900

    Main JS

---
+++ FirstJS/Chapter03/app.js
@@ -0,0 +1,17 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+function inLoginSubmit(event) {
+    event.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    console.log(username);
+//    greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.addEventListener("submit", inLoginSubmit);
