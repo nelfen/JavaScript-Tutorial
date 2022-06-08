ac772ab4921567eb67fcfaf28ae144804215bf8c
nelfen <nelfen.dev@gmail.com>
2022-06-08 21:51:33 +0900

    Return 예제 - Int값 변환

--- FirstJS/Chaper01/Return(parseInt).js
+++ FirstJS/Chaper01/Return(parseInt).js
@@ -1,4 +1,7 @@
-const age = parseInt(prompt("How old are you?"));
+//const age = parseInt(prompt("How old are you?"));
+const age = prompt("How old are you?");
 
 console.log(age);
 console.log(typeof age);
+console.log(parseInt(age));
+console.log(typeof parseInt(age))
