ebf635e93f00f5fb6d4b0a11597a274afe1e76af
nelfen <nelfen.dev@gmail.com>
2022-06-09 21:38:47 +0900

    Main HTML

--- FirstJS/Chapter02/HTMLEdit(More).js
+++
@@ -1,7 +0,0 @@
-//const hellos = document.getElementsByClassName("hello");
-//const title = document.getElementsByTagName("h1");
-//const title = document.querySelectorAll("div h1");
-const title = document.querySelector(".hello h1");
-
-//console.log(title);
-title.innerText = "Hello";
