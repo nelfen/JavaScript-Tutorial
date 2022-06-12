48d6882981c8494df6b176744a0a99942e76c65e
nelfen <nelfen.dev@gmail.com>
2022-06-12 11:52:48 +0900

    Background JS

--- FirstJS/Momentum/js/background.js
+++ FirstJS/Momentum/js/background.js
@@ -2,14 +2,14 @@ const images = ["0.png", "1.png", "4.png", "6.png"];
 
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenImage}`;
+//const bgImage = document.createElement("img");
+//bgImage.src = `img/${chosenImage}`;
+//document.body.appendChild(bgImage);
 
 document.body.style.backgroundImage = `url(img/${chosenImage})`;
 
 if (chosenImage === "0.png") {
     document.body.style.color = "black";
 } else {
-
+    document.body.style.color = "white";
 }
