0920027e3f5fb5339fe6586484616111671dd289
nelfen <nelfen.dev@gmail.com>
2022-06-12 21:59:09 +0900

    Weather JS

--- FirstJS/Momentum/js/weather.js
+++ FirstJS/Momentum/js/weather.js
@@ -3,11 +3,10 @@ const API_KEY = "c3f5470a4d9da2691cdab4ab8ff29f98";
 function onGeoOK(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    console.log("You live in", lat, lon);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
     fetch(url)
-        .then((Response) => Response.json())
-        .then((data) => {
+        .then((Response) => Response.json())    //필수코드(?)
+        .then((data) => {   //url에서 받은 데이터 사용
                 const weather = document.querySelector("#weather span:first-child");
                 const city = document.querySelector("#weather span:last-child");
                 city.innerText = data.name;
