ea0d5b2a9b4d06fa2a177403fa4a87cc5a0160ca
nelfen <nelfen.dev@gmail.com>
2023-08-08 16:42:31 +0900

    객체 예제 - 가위바위보 맞추기

--- DoitJSJQ/JS/objEx2.js
+++ DoitJSJQ/JS/objEx2.js
@@ -1,25 +1,3 @@
-var game = prompt("가위, 바위, 보 중 선택하세요", "가위");
-var gameNum;
-switch(game) {
-  case "가위":
-    gameNum = 1; break;
-  case "바위":
-    gameNum = 2; break;
-  case "보":
-    gameNum = 3; break;
-  default: alert("잘못 작성했습니다.");
-    location.reload();  
-}
-
-var com = Math.ceil(Math.random()*3);
-document.write("<img src=\"images/math_img_" + com + ".jpg\"><br>");
-
-if (gameNum == com) {
-  document.write("<img src=\"images/game_1.jpg\"><br>");
-} else {
-  document.write("<img src=\"images/game_2.jpg\"><br>");
-}
-
 var d = new Array();
 d[0] = "male";
 d[1] = 23;
